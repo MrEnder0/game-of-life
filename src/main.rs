de04fa661bc99b0307ca16735a96fab798e76bca
MrEnder0 <endermobtrey@gmail.com>
2023-02-17 08:54:27 -0600

    base grid gen

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+    let mut rng = rand::thread_rng();
+    let mut main_layer = [[0u8; 10]; 10];
+    let mut possible_layer = [[0u8; 10]; 10];
+
+    main_layer[0][rng.gen_range(0..10)] = 1;
+
+    //prints out the main layer in a more readable format
+    for i in 0..10 {
+        for j in 0..10 {
+            print!("{}", main_layer[i][j]);
+        }
+        println!();
+    }
 }
