5e35fb0370632977f0c16ea06741a8e0d00d59c6
MrEnder0 <endermobtrey@gmail.com>
2023-02-17 15:48:29 -0600

    updated setting var names

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,25 @@ use ini::Ini;
 
 fn main() {
     let settings = Ini::load_from_file("settings.ini").unwrap();
-    let size = settings.get_from(Some("settings"), "size").unwrap().parse::<usize>().unwrap();
+    let frame_size = settings.get_from(Some("settings"), "frame_size").unwrap().parse::<usize>().unwrap();
+    let frame_delay = settings.get_from(Some("settings"), "frame_delay").unwrap().parse::<u64>().unwrap();
+    let spawn_multiplier = settings.get_from(Some("settings"), "spawn_multiplier").unwrap().parse::<usize>().unwrap();
     let filled = settings.get_from(Some("settings"), "filled").unwrap();
     let empty = settings.get_from(Some("settings"), "empty").unwrap();
-    let delay = settings.get_from(Some("settings"), "delay").unwrap().parse::<u64>().unwrap();
 
     let mut rng = rand::thread_rng();
-    let mut main_layer = vec![vec![0; size]; size];
-    let mut possible_layer = vec![vec![0; size]; size];
+    let mut main_layer = vec![vec![0; frame_size]; frame_size];
+    let mut possible_layer = vec![vec![0; frame_size]; frame_size];
 
-    for _i in 0..size*20 {
-        let init_pos_x = rng.gen_range(1..size-1);
-        let init_pos_y = rng.gen_range(1..size-1);
+    for _i in 0..frame_size*spawn_multiplier {
+        let init_pos_x = rng.gen_range(1..frame_size-1);
+        let init_pos_y = rng.gen_range(1..frame_size-1);
         main_layer[init_pos_x][init_pos_y] = 1;
     }
 
     while 1==1 {
-        for x in 1..size-1 {
-            for y in 1..size-1 {
+        for x in 1..frame_size-1 {
+            for y in 1..frame_size-1 {
                 let mut _neighbours = 0;
 
                 _neighbours = main_layer[x-1][y-1] + main_layer[x-1][y] + main_layer[x-1][y+1] + main_layer[x][y-1] + main_layer[x][y+1] + main_layer[x+1][y-1] + main_layer[x+1][y] + main_layer[x+1][y+1];
@@ -50,8 +51,8 @@ fn main() {
             }
         }
         
-        for x in 1..size-1 {
-            for y in 1..size-1 {
+        for x in 1..frame_size-1 {
+            for y in 1..frame_size-1 {
                 if possible_layer[x][y] == 1 {
                     main_layer[x][y] = 1;
                     possible_layer[x][y] = 0;
@@ -65,12 +66,12 @@ fn main() {
         
         // clear previous frame
         print!("{}[2J", 27 as char);
-        for x in 1..size-1 {
-            for y in 1..size-1 {
+        for x in 1..frame_size-1 {
+            for y in 1..frame_size-1 {
                 print!("{}", main_layer[x][y].to_string().replace("0", empty).replace("1", filled));
             }
             println!("");
         }
-        thread::sleep(std::time::Duration::from_millis(delay));
+        thread::sleep(std::time::Duration::from_millis(frame_delay));
     }
 }
