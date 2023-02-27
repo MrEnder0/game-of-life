0c40dc81c3c81b6f19e090c2225b11eb6baab281
MrEnder0 <endermobtrey@gmail.com>
2023-02-27 00:42:21 -0600

    better error finding in config

--- src/config_manager.rs
+++ src/config_manager.rs
@@ -29,11 +29,8 @@ pub(crate) fn load_config() -> (usize, u64, usize, char, char, u64, bool, bool) 
     let use_seed = settings.get_from(Some("settings"), "use_seed").unwrap().parse::<bool>().unwrap();
     let interleaved_frames = settings.get_from(Some("settings"), "interleaved_frames").unwrap().parse::<bool>().unwrap();
 
-    if empty_tile == filled_tile {
-        println!("Filled tile and empty tile cannot be the same; please change them in settings.ini; Exiting in 5 seconds...");
-        thread::sleep(std::time::Duration::from_millis(5000));
-        std::process::exit(0);
-    }
+    assert!(frame_size > 2, "Frame size must be greater than 2; please change them in settings.ini; Exiting...");
+    assert!(empty_tile == filled_tile, "Filled tile and empty tile cannot be the same; please change them in settings.ini; Exiting...");
 
     return (frame_size, frame_delay, spawn_multiplier, filled_tile, empty_tile, starting_seed, use_seed, interleaved_frames);
 }
