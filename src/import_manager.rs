5e90da547b022fb8ec9acf06f901603258a8392a
MrEnder0 <endermobtrey@gmail.com>
2023-03-07 21:25:36 -0600

    reads life file with ver

--- src/import_manager.rs
+++ src/import_manager.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::thread;
 
 pub(crate) fn check_imports() -> bool {
     let args: Vec<String> = env::args().collect();
@@ -15,5 +16,25 @@ pub(crate) fn check_imports() -> bool {
 }
 
 pub(crate) fn parse_import() {
-    //read file and do stuff soon
+    let args: Vec<String> = env::args().collect();
+    let filename = &args[1];
+    let file = std::fs::read_to_string(filename).unwrap();
+    let lines = file.lines();
+    
+    let mut iter = lines.into_iter();
+    while let Some(line) = iter.next() {
+        if line.starts_with('%') && &line[1..4] == "VER" {
+            match line[5..].parse::<i32>().unwrap() {
+                1 => {
+                    let frame = iter.next().unwrap().split('|').collect::<Vec<_>>();
+                    for row in frame {
+                        println!("{}", row);
+                    }
+                }
+                _ => println!("Loaded unknown life version ignoring..."),
+            }
+        }
+    }
+    
+    thread::sleep(std::time::Duration::from_millis(3000));
 }
