9e7e2d0c1bbad2940bef12207693f5920ac1840c
MrEnder0 <endermobtrey@gmail.com>
2023-03-08 18:19:16 -0600

    added life 1.05 and 1.06

--- src/import_manager.rs
+++ src/import_manager.rs
@@ -39,7 +39,61 @@ pub(crate) fn parse_import() {
                         println!("{}", row.replace(".", "0").replace("#", "1"));
                     }
                 }
-                _ => println!("Loaded unknown life format found only supports 'ELFv1', and 'ELFv2'..."),
+                "LIFEv1.05" => {
+                    let mut itter_clone = iter.clone();
+                    itter_clone.next();
+                    let frame: Vec<String> = itter_clone.map(|s| s.to_string()).collect();
+                    for row in &frame {
+                        println!("{}", row.replace(".", "0").replace("*", "1"));
+                    }
+                }
+                "LIFEv1.06" => {
+                    let mut itter_clone = iter.clone();
+                    let mut width = 0;
+                    let mut height = 0;
+                    
+                    // skip the first line of the input
+                    itter_clone.next();
+                    
+                    // read in the coordinates and update the width and height
+                    for line in itter_clone {
+                        let coords: Vec<i32> = line
+                            .split_whitespace()
+                            .map(|s| s.parse().unwrap())
+                            .collect();
+                        let x = coords[0];
+                        let y = coords[1];
+                    
+                        if x.abs() > width {
+                            width = x.abs();
+                        }
+                        if y.abs() > height {
+                            height = y.abs();
+                        }
+                    }
+                    
+                    // create the grid and set the cells based on the coordinates
+                    let mut grid = vec![vec![0; (width * 2 + 1) as usize]; (height * 2 + 1) as usize];
+                    let iter_clone = iter.clone();
+                    for line in iter_clone {
+                        let coords: Vec<i32> = line
+                            .split_whitespace()
+                            .map(|s| s.parse().unwrap())
+                            .collect();
+                        let x = coords[0];
+                        let y = coords[1];
+                        grid[(y + height) as usize][(x + width) as usize] = 1;
+                    }
+                    
+                    // print out the grid
+                    for row in &grid {
+                        for cell in row {
+                            print!("{}", cell);
+                        }
+                        println!();
+                    }   
+                }
+                _ => println!("Loaded unknown life format found only supports 'ELFv1', 'ELFv2', 'LIFEv1.05', and 'LIFEv1.06'...")
             }
         }
     }
